72c26a6bf5a9f15b20209a8f2ad2f3d7be89036b
Nathan <nathan.lauth@viacesi.fr>
2020-04-28 18:37:55 +0200

    Add material angular, login and presentation component

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {PresentationComponent} from './gui/presentation/presentation.component';
+import {LoginComponent} from './gui/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'presentation',
+    pathMatch: 'full'
+  },
+  {
+    path: 'presentation',
+    component: PresentationComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -7,17 +7,29 @@ import {AccentFoldPipe} from './pipes/accent-fold.pipe';
 import {ToastyModule} from 'ng2-toasty';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {TokenInterceptor} from './interceptor/token.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { PresentationComponent } from './gui/presentation/presentation.component';
+import { LoginComponent } from './gui/login/login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccentFoldPipe
+    AccentFoldPipe,
+    PresentationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ToastyModule.forRoot(),
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [
     {
